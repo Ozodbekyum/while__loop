6892577464644ae551463ea92f40cccfc608ca10
Ozodbekyum <hello7ozod@gamil.com>
2022-08-29 10:20:56 +0500

    while18

--- bin/while__loop18.dart
+++ bin/while__loop18.dart
@@ -1,3 +1,10 @@
 void main() {
+  int x = 0;
+  for (int i = 50; i < 1000; i++) {
+    if (i % 2 == 0) {
+      x += i;
+    }
+  }
+  print(x);
   //Print the sum of pairs of numbers between 50 and 1000
 }
