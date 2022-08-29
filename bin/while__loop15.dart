b86b63740e17174ab12e574b7f500a18f233a86f
Ozodbekyum <hello7ozod@gamil.com>
2022-08-29 10:01:59 +0500

    while15

--- bin/while__loop15.dart
+++ bin/while__loop15.dart
@@ -1,3 +1,10 @@
 void main() {
+  int y = 0;
+  for (int i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+      y += i;
+    }
+  }
+  print(y);
   //Print the sum of pairs of numbers from 1 to 100
 }
