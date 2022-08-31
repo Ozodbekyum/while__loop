dd9f213ee23d17d131c7e144cdb9b1461d1f1352
Ozodbekyum <hello7ozod@gamil.com>
2022-08-31 10:10:26 +0500

    while21

--- bin/while__loop21.dart
+++ bin/while__loop21.dart
@@ -1,7 +1,9 @@
 void main() {
   int x = 1;
   for (int i = 12; i < 34; i++) {
-    x *= i;
+    if (i % 2 == 0) {
+      x *= i;
+    }
   }
   print(x);
   //Print the product of even numbers from 100  to 345
